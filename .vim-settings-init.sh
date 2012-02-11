fa22a5ee497fef9cd848415c7209610ebf564897
Xian Xu <xx@twitter.com>
2012-02-11 15:54:48 -0800

    init script

---
+++ .vim-settings-init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# overlaying other repositories on top
+echo Getting plugins
+git clone https://github.com/wincent/Command-T.git .vim/bundle/command-t
+git clone https://github.com/Shougo/neocomplcache .vim/bundle/neocomplcache
+git clone https://github.com/scrooloose/nerdcommenter.git .vim/bundle/nerdcommenter
+git clone https://github.com/tpope/vim-fugitive.git .vim/bundle/vim-fugitive
+git clone https://github.com/derekwyatt/vim-scala.git .vim/bundle/vim-scala
+
+echo Building natives
+pushd ~/.vim/bundle/command-t/ruby/command-t
+ruby extconf.rb
+make
+popd
